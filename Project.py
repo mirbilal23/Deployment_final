99b38fe6dfdbc32772a238da91feef9410808969
mirbilal23 <133589654+mirbilal23@users.noreply.github.com>
2024-04-12 11:58:44 -0400

    Create Project.py

---
+++ Project.py
@@ -0,0 +1,10 @@
+#Organizing imports here--------------
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+data = pd.read_csv('housing.csv')
+data
